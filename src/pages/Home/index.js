54f424be8783577f3402c47379bdbe0cac599425
Thallys Cézar <thalys_cezar@hotmail.com>
2022-11-03 09:20:07 -0300

    refactor: add provider in the home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,13 @@
 import Card from "../../components/Card";
 import NavBar from "../../components/NavBar";
+import { WishListProvider } from "../../hooks/context";
 
 const Home = () => {
   return (
-    <>
+    <WishListProvider>
       <NavBar />
       <Card />
-    </>
+    </WishListProvider>
   );
 };
 
